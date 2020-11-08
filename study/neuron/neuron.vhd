5136a56f9dbcfc9a980bd4c41578e21362b71a9d
Adenilson Castro <adenilsoncastro@hotmail.com.br>
2020-11-07 21:16:36 -0300

    lut updated; neuron with relu

--- study/neuron/neuron.vhd
+++ study/neuron/neuron.vhd
@@ -25,7 +25,8 @@
   
  architecture rtl of neuron is
  
-	type t_sm is (s_idle, s_get_weight, s_wait_weight, s_mac, s_wait_mac, s_mac_result, s_act_func, s_clear);
+	--Control FSM signals
+	type t_sm is (s_idle, s_get_weight, s_wait_weight, s_mac, s_wait_mac, s_mac_result, s_relu, s_wait_relu, s_clear);
 	signal r_sm					: t_sm := s_idle;
 	signal r_sinapse_count	: integer := 0;
 	signal r_done				: std_logic := '0';
@@ -41,6 +42,11 @@
 	signal r_mac_enable		: std_logic												:= '0';
 	signal r_mac_done			: std_logic												:= '1';
 	signal r_mac_out			: std_logic_vector(g_bits_data-1 downto 0)	:= (others => '0'); 
+	--ReLu LUT signals
+	signal r_relu_enable		: std_logic												:= '0';
+	signal r_relu_in			: std_logic_vector(g_bits_data-1 downto 0)	:= (others => '0'); 
+	signal r_relu_out			: std_logic_vector(g_bits_data-1 downto 0)	:= (others => '0'); 
+	
 	
 	component ram is
 		generic(
@@ -70,11 +76,24 @@
 			o_done		: out std_logic;
 			o_data		: out std_logic_vector(g_bits-1 downto 0));
 	end component;
+	
+	component lut_relu is
+		generic(
+			g_bits		: natural := 16;
+			g_fxp_high	: natural := 4;
+			g_fxp_low	: integer := -11);
+		port(
+			i_clk			: in std_logic;
+			i_enable		: in std_logic;
+			i_value		: in std_logic_vector(g_bits-1 downto 0);
+			o_result 	: out std_logic_vector(g_bits-1 downto 0));
+	end component;
 	 
  begin
  
-	ram_n1 : ram port map(i_clk, r_wr, r_addr, r_data_in_ram, r_data_out_ram);
-	mac_n1 : mac port map(i_clk, i_rst, r_mac_enable, i_fxp_data, r_data_out_ram, r_mac_done, r_mac_out);
+	ram_n1 	: ram port map(i_clk, r_wr, r_addr, r_data_in_ram, r_data_out_ram);
+	mac_n1 	: mac port map(i_clk, i_rst, r_mac_enable, i_fxp_data, r_data_out_ram, r_mac_done, r_mac_out);
+	act_relu	: lut_relu port map(i_clk, r_relu_enable, r_relu_in, r_relu_out);
 	
 	p_neuron : process(i_clk, i_enable, r_mac_done)
 	begin
@@ -82,7 +101,6 @@
 			if i_enable = '1' then
 				case r_sm is
 					when s_idle =>
-						r_done <= '0';
 						if i_enable = '0' then
 							r_sm <= s_idle;
 						else
@@ -110,20 +128,27 @@
 							if r_sinapse_count < c_neurons then
 								r_sm <= s_get_weight;
 							else
-								r_sm <= s_act_func;
+								r_sm <= s_relu;
 							end if;
 						else
 							r_sm <= s_mac;
 						end if;
 					
-					when s_act_func =>
-						r_mac_enable 		<= '0';					
-						r_sm 			 <= s_clear;
+					when s_relu =>
+						r_mac_enable	<= '0';
+						r_relu_enable 	<= '1';
+						r_relu_in		<= r_mac_out;
+						r_sm 			 	<= s_wait_relu;
+					
+					when s_wait_relu =>
+						r_relu_enable 	<= '0';
+						r_done			<= '1';
+						r_sm 				<= s_clear;
 					
 					when s_clear =>
+						r_done 				<= '0';
 						r_sinapse_count 	<= 0;
 						r_mac_enable 		<= '0';
-						r_done				<= '1';
 						r_sm 					<= s_idle;
 						
 					when others =>
@@ -135,6 +160,6 @@
 	
 	o_mac_done	<= r_mac_done;
 	o_done		<= r_done;
-	o_fxp_data 	<= r_mac_out;
+	o_fxp_data 	<= r_relu_out;
   
  end rtl;
