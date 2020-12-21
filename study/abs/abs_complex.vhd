0701385b487db48a2f0c00992cf2941790a4f98a
Adenilson Castro <adenilsoncastro@hotmail.com.br>
2020-12-21 17:09:08 -0300

    abs updated; features started

--- study/abs/abs_complex.vhd
+++ study/abs/abs_complex.vhd
@@ -5,28 +5,34 @@
  use ieee_proposed.fixed_pkg.all;
  
  library rna_library;
- use rna_library.data_types_pkg.t_complex;
+ use rna_library.data_types_pkg.all;
  
  
- entity abs_complex is		
+ entity abs_complex is	
+	generic(
+		g_bits			: natural := c_bits;
+		g_fxp_high		: natural := c_fxp_high;
+		g_fxp_low		: integer := c_fxp_low);
 	port(
 		i_clk				: in std_logic;
 		i_enable			: in std_logic;
-		i_real			: in std_logic_vector(15 downto 0);
-		i_img				: in std_logic_vector(15 downto 0);
-		o_complex		: out std_logic_vector(15 downto 0));
+		i_real			: in std_logic_vector(g_bits-1 downto 0);
+		i_img				: in std_logic_vector(g_bits-1 downto 0);
+		o_done			: out std_logic;
+		o_complex		: out std_logic_vector(g_bits-1 downto 0));
  end abs_complex;
  
  
  architecture bhv of abs_complex is
+	type t_sm is (s_idle, s_squared, s_sum, s_clear);
+	signal r_sm : t_sm := s_idle;
 	
-	signal r_real 				: sfixed(4 downto -11) := (others => '0');
-	signal r_real_squared	: sfixed(4 downto -11) := (others => '0');
-	
-	signal r_img  				: sfixed(4 downto -11) := (others => '0');
-	signal r_img_squared		: sfixed(4 downto -11) := (others => '0');
-	
-	signal r_sum				: sfixed(4 downto -11) := (others => '0');
+	signal r_real 				: sfixed(g_fxp_high downto g_fxp_low) := (others => '0');
+	signal r_real_squared	: sfixed(g_fxp_high downto g_fxp_low) := (others => '0');	
+	signal r_img  				: sfixed(g_fxp_high downto g_fxp_low) := (others => '0');
+	signal r_img_squared		: sfixed(g_fxp_high downto g_fxp_low) := (others => '0');	
+	signal r_sum				: sfixed(g_fxp_high downto g_fxp_low) := (others => '0');
+	signal r_done				: std_logic := '0';
 	
  begin
  
@@ -37,16 +43,37 @@
 		r_img  <= to_sfixed(i_img, r_img'high, r_img'low);
 		
 		if rising_edge(i_clk) then
-			if i_enable = '1' then
-				r_real_squared <= resize(r_real*r_real, r_real_squared'high, r_real_squared'low); 
-				r_img_squared 	<= resize(r_img*r_img, r_img_squared'high, r_img_squared'low);		
-				r_sum 			<= resize(r_real_squared + r_img_squared, r_sum'high, r_sum'low);
+			case r_sm is
+				when s_idle =>
+					if i_enable = '1' then
+						r_sm <= s_squared;
+					else
+						r_sm <= s_idle;
+					end if;
 				
-				--sqrt of r_sum ?
-				
-			end if;
+				when s_squared =>
+					r_real_squared <= resize(r_real*r_real, r_real_squared'high, r_real_squared'low); 
+					r_img_squared 	<= resize(r_img*r_img, r_img_squared'high, r_img_squared'low);		
+					r_sm <= s_sum;
+					
+				when s_sum =>
+					r_sum	<= resize(r_real_squared + r_img_squared, r_sum'high, r_sum'low);
+					r_done <= '1';
+					r_sm 	<= s_clear;
+					
+				when s_clear =>
+					r_real_squared <= (others => '0');
+					r_img_squared 	<= (others => '0');
+					r_done 			<= '0';
+					r_sm 				<= s_idle;
+					
+				when others => 
+					r_sm <= s_idle;
+					
+			end case;				
 		end if;	
 	end process p_abs;
 	
- o_complex <= to_slv(r_sum);
+ o_done 		<= r_done;	
+ o_complex 	<= to_slv(r_sum);
  end bhv;
--- study/abs/abs_complex_tb.vhd
+++ study/abs/abs_complex_tb.vhd
@@ -5,7 +5,7 @@
  use ieee_proposed.fixed_pkg.all;
  
  library rna_library;
- use rna_library.data_types_pkg.t_complex;
+ use rna_library.data_types_pkg.all;
  
  entity abs_complex_tb is
  end abs_complex_tb;
@@ -17,6 +17,7 @@
  signal r_real		: std_logic_vector(15 downto 0) := (others => '0');
  signal r_img		: std_logic_vector(15 downto 0) := (others => '0');
  signal r_result	: std_logic_vector(15 downto 0) := (others => '0');
+ signal r_done 	: std_logic := '0';
  
  
  component abs_complex is
@@ -25,23 +26,24 @@
 		i_enable			: in std_logic;
 		i_real			: in std_logic_vector(15 downto 0);
 		i_img				: in std_logic_vector(15 downto 0);
+		o_done			: out std_logic;
 		o_complex		: out std_logic_vector(15 downto 0));
  end component;
  
  
  begin
  
- dut : abs_complex port map(r_clk, r_enable, r_real, r_img, r_result);
+ dut : abs_complex port map(r_clk, r_enable, r_real, r_img, r_done, r_result);
  
  r_clk <= not r_clk after 20 ns;
  
  p_test : process
  begin
-	 r_real 		<= "0000000110010011";
+	 r_real 	<= "0000000110010011";
 	 r_img	<= "1111110001111101";
 	 
 	 r_enable <= '1';
-	 wait for 200 ns;
+	 wait until r_done = '1';
 	 r_enable <= '0';
 	 
 	assert false report "Test finished" severity failure;
